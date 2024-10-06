57c35628968ae153ea30297a9dc126f9bc6a46f6
ilyasseyounes <ilyasseyounes1@gmail.com>
2024-10-06 12:49:06 +0100

    C SingleLL 2

--- LinkedList/SingleLL.c
+++ LinkedList/SingleLL.c
@@ -1,20 +1,83 @@
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-
-typedef struct Node {
+struct Node {
     int data;
-    struct Node* link;
+    struct Node* next;
 };
 
+struct Node* head = NULL;
 
-typedef struct Node Node;
+// Print function to print the entire list
+void Print() {
+    struct Node* temp = head;
+    printf("List is: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
 
+// Insert function to add a new node at the beginning
+void Insert(int x) {
+    struct Node* temp = malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->next = head;
+    head = temp;
+}
+void InsertAfter (int x,int index){
+    struct Node* node = malloc(sizeof (struct Node));
+    struct Node* temp = head;
+    if (head->next == NULL){
+        Insert(x);
+        return;
+    }
+    while (index != 0 ){
+        temp = temp ->next;
+        index--;
+    }
+    node -> data = x;
+    node -> next = temp -> next;
+    temp -> next = node ;
+
+}
+void Delet (int index){
+    struct Node* node = malloc(sizeof(struct Node));
+
+    while (index != 0){
+        node = node ->next;
+        index--;
+    }
+    if (node == head){
+        head = head -> next;
+    }else if (node -> next -> next == NULL){
+        node -> next = NULL;
+    }else
+    node -> next = node -> next -> next;
+
+}
 int main() {
-    struct Node A;
-    struct Node temp = *( Node* ) malloc(sizeof(Node));
-    temp.data = 2;
-    temp.link = 0;
-    A = temp;
+    int n, x, i;
+
+    printf("How many numbers?\n");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        printf("Enter the number: ");
+        scanf("%d", &x);
+        Insert(x);
+        Print();
+    }
+
+    InsertAfter(12,2);
+    InsertAfter (13,4);
+    Print();
+    Delet(2);
+    Print();
+
     return 0;
 }
+
